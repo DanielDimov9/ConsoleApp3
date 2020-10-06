2d5131e64e39522d503ea651f7a9449f85cd02b4
DanielBonesDimov DanielBonesDimov <danieldimov10g@abv.bg>
2020-10-06 18:09:47 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime date = DateTime.Now;
+
+            Console.Write("Current date:");
+
+            Console.WriteLine(date.ToShortDateString());
+
+            Console.Write("Current time:");
+
+            Console.WriteLine(date.ToShortTimeString());
+
+            Console.Write("Date after 10 years");
+            Console.WriteLine(date.AddYears(10).ToShortDateString());
+
+            Console.Write("Please enter period: ");
+            int period = int.Parse(Console.ReadLine());
+
+
+
+        }
+    }
+}
+
